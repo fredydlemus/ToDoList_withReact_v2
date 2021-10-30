da6e8b50f3499a8c2ba6df05dc29be851fac067f
fredyflemus <fredyflemus@gmail.com>
2021-10-30 10:15:12 -0600

    create modal

--- src/components/ToDoModal.jsx
+++ src/components/ToDoModal.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import reactDom from 'react-dom';
+import AppContext from '../context/AppContext';
+import '../styles/ToDoModal.scss';
 
-const Modal = ({children}) =>{
+const Modal = () =>{
 
+    const {modifyModal} = React.useContext(AppContext);
     
+    const onCancel = (event) =>{
+        event.stopPropagation();
+        if(event.target.className === 'Modal' || event.target.className === 'cancel-button'){
+            console.log(event.target.className)
+            modifyModal();
+        }
+        
+    }
 
     return reactDom.createPortal(
-        <div className='Modal' >
-            {children}
-            <h1>Soy el modal :D</h1>
+        <div 
+            className='Modal'
+            onClick={onCancel} >
+            
+                <form className='Modal-form'>
+                    <label>Write a new ToDo</label>
+                    <textarea
+                        placeholder='Do math homework'
+                    ></textarea>
+                    <div className='Modal-buttons'>
+                        <button className='cancel-button'
+                            type='button'
+                            onClick={onCancel}
+                        >Cancel</button>
+                        <button
+                            type='button'
+                        >Add</button>
+                    </div>
+                </form>
+            
+            
         </div>,
         document.getElementById('modal')
     );
