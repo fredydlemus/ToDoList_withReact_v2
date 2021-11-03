be66677ecdd4c0f17fa933b36a88c9d88de5fc2b
fredyflemus <fredyflemus@gmail.com>
2021-11-03 00:48:36 -0600

    [add] case on empty todos

--- src/containers/ToDoList.jsx
+++ src/containers/ToDoList.jsx
@@ -2,29 +2,36 @@ import React from 'react'
 import ToDoItem from '../components/ToDoItem';
 import '../styles/ToDoList.scss';
 import AppContext from '../context/AppContext';
+import CreateNewTodo from '../components/CreateNewTodo';
 
 
 
 
 const ToDoList = () =>{
 
-    const {searchedTodos} = React.useContext(AppContext);
+    const {searchedTodos, totalTodos} = React.useContext(AppContext);
     
     const todos = searchedTodos;
     
 
     return(
         <section className='ToDoList'>
-            <ul>
-                {todos.map(todo => (
+            {!totalTodos ? 
+                <div className="ToDoList-empty">
+                    <p>Create your first ToDo</p>
+                    <CreateNewTodo />
+                </div> : 
+                <ul>
+                    {todos.map(todo => (
                     <ToDoItem 
                         key = {todo.id}
                         id={todo.id}
                         text = {todo.text}
                         completed = {todo.completed}
                     />
-                ))}
-            </ul>
+                ))}    
+                </ul>}
+            
         </section>
     );
 }
